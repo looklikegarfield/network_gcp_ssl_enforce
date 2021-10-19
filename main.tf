ac2c37e38da3f03250200865b464dc4446a1ca4e
looklikegarfield <looklikegarfieldd@gmail.com>
2021-10-19 11:25:09 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,41 +1,63 @@
-resource "google_dataproc_cluster" "simplecluster" {
-  name   = "simplecluster"
-  region = "us-central1"
+
+
+provider "google-beta" {
+  access_token = var.access_token
 }
 
-resource "google_service_account" "default" {
-  account_id   = "service-account-id"
+resource "google_service_account" "dataproc-sa" {
+  account_id   = "dataproc-sa-id"
   display_name = "Service Account"
+  project      = var.project_id
 }
 
-resource "google_dataproc_cluster" "mycluster" {
-  name     = "mycluster"
+resource "google_project_iam_member" "dataproc-worker" {
+  project = var.project_id
+  role    = "roles/dataproc.worker"
+  member  = "serviceAccount:${google_service_account.dataproc-sa.email}"
+}
+
+resource "google_dataproc_cluster" "cluster-second" {
+  name     = "cluster-second"
+  provider = google-beta
+  project  = var.project_id
   region   = "us-central1"
+
   graceful_decommission_timeout = "120s"
+
   labels = {
-    foo = "bar"
+    gcp_region           = "us-central1"
+    owner                = "wf"
+    application_division = "pci"
+    application_role     = "auth"
+    environment          = "dev"
   }
 
   cluster_config {
-    staging_bucket = "dataproc-staging-bucket"
+    staging_bucket = "dataproc-staging-bucket123"
+
+    gce_cluster_config {
+      tags             = ["foo", "bar"]
+      internal_ip_only = true
+
+      #network    = "projects/loyal-network-323915/global/networks/test-network"
+      subnetwork = "projects/loyal-network-323915/regions/us-central1/subnetworks/test-network"
+
+      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+      service_account = google_service_account.dataproc-sa.email
+      service_account_scopes = [
+        "cloud-platform"
+      ]
+    }
 
     master_config {
       num_instances = 1
       machine_type  = "e2-medium"
-      disk_config {
-        boot_disk_type    = "pd-ssd"
-        boot_disk_size_gb = 30
-      }
     }
 
     worker_config {
-      num_instances    = 2
-      machine_type     = "e2-medium"
-      min_cpu_platform = "Intel Skylake"
-      disk_config {
-        boot_disk_size_gb = 30
-        num_local_ssds    = 1
-      }
+      num_instances = 2
+      machine_type  = "e2-medium"
+      #min_cpu_platform = "Intel Skylake"
     }
 
     preemptible_worker_config {
@@ -50,19 +72,77 @@ resource "google_dataproc_cluster" "mycluster" {
       }
     }
 
-    gce_cluster_config {
-      tags = ["foo", "bar"]
-      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-      service_account = google_service_account.default.email
-      service_account_scopes = [
-        "cloud-platform"
-      ]
+
+    encryption_config {
+      kms_key_name = ""
     }
 
-    # You can define multiple initialization_action blocks
-    initialization_action {
-      script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
-      timeout_sec = 500
+    endpoint_config {
+      enable_http_port_access = false
+    }
+  }
+  depends_on = [google_service_account.dataproc-sa, google_project_iam_member.dataproc-worker]
+}
+
+resource "google_dataproc_cluster" "cluster-wsae" {
+  name     = "cluster-wsae"
+  provider = google-beta
+  project  = var.project_id
+  region   = "us-east1"
+
+  graceful_decommission_timeout = "120s"
+
+  labels = {
+    gcp_region           = "us-central1"
+    owner                = "wf"
+    application_division = "pci"
+    application_role     = "auth"
+    environment          = "dev"
+  }
+
+  cluster_config {
+    staging_bucket = "dataproc-staging-bucket123"
+
+    gce_cluster_config {
+      tags             = ["foo", "bar"]
+      internal_ip_only = true
+
+      #network    = "projects/loyal-network-323915/global/networks/test-network"
+      subnetwork = "projects/loyal-network-323915/regions/us-west1/subnetworks/test-network"
+
+      # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+      service_account = "abhishek@loyal-network-323915.iam.gserviceaccount.com"
+    }
+
+    master_config {
+      num_instances = 1
+      machine_type  = "e2-medium"
+    }
+
+    worker_config {
+      num_instances = 2
+      machine_type  = "e2-medium"
+      #min_cpu_platform = "Intel Skylake"
+    }
+
+    preemptible_worker_config {
+      num_instances = 0
+    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "1.3.7-deb9"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+    }
+
+    encryption_config {
+      kms_key_name = ""
+    }
+
+    endpoint_config {
+      enable_http_port_access = false
     }
   }
 }
